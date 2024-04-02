67abe3eee43c12734de4865f6662316a04c0f2bc
scio_station <114015214+sciostation@users.noreply.github.com>
2024-04-02 14:44:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@
 
 //==[ 1. BASIC INFO ]==
 
-let blogName = "My Blog Name";
-let authorName = "My Name Here";
-let authorLink = ""; // Enter your website, social media, etc. Some way for people to tell you they like your blog! (Leaving it empty is okay too)
+let blogName = "Blog Scio Medios";
+let authorName = "scio medios";
+let authorLink = "https://sciostation.github.io/"; // Enter your website, social media, etc. Some way for people to tell you they like your blog! (Leaving it empty is okay too)
 
 //-----------------------------
 
